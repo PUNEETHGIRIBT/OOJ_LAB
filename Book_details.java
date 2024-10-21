fd8fbb843eef6d6834f871a9422e27e9dcff4f0e
PUNEETHGIRIBT <153577873+PUNEETHGIRIBT@users.noreply.github.com>
2024-10-21 23:48:20 +0530

    Update Book_details.java

--- Book_details.java
+++ Book_details.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.Scanner;
 
 class Book{
 	String name;
@@ -24,12 +25,48 @@ class Book{
 class Book_details{
 	public static void main(String args[]){
 	Scanner sc = new Scanner(System.in);
-	Book b1 = new Book();
-	b1.set_val("C_Program","Jez",150,350);
-	b1.get_val();
+	System.out.println("Enter number of Books");
+	int n = sc.nextInt();
+
+	Book[] books = new Book[n];
+
+	for(int i=0; i<n; i++){
+		books[i] = new Book();
+		System.out.println("\nEnter details of Book :"+(i+1));
+
+		if(i==0){
+			sc.nextLine();
+		}
+		System.out.println("Enter Name: ");
+		String name = sc.nextLine();
+
+		System.out.println("Enter author:");
+		String author = sc.nextLine();
+
+		System.out.println("Enter price in rupees:");
+		int price = sc.nextInt();
+
+		System.out.println("Enter number of pages:");
+		int num_pages = sc.nextInt();
+		sc.nextLine();
+		
+		books[i].set_val(name,author,price,num_pages);
+		}
+
+	for(int i=0; i<n; i++){
+		System.out.println("\nDetails of Book "+(i+1));
+		books[i].get_val();
+	    }
 
 	}
 }
+	
+
+
+
+			
+	
+	
 	
 
 
